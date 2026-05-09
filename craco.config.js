86b78338e895ff6e524d01fb9eea00de463eabff
Rajat <rjtguptaji@gmail.com>
2026-05-09 10:45:22 +0530

    fix: use craco to resolve fs module issue

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        fs: false,
+        path: false,
+        os: false,
+      };
+      return webpackConfig;
+    },
+  },
+};
